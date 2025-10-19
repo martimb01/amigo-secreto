c425289d2dbf491b13b7c062a62609a624a598b2
Martim-Desktop <martim.souto.bento@gmail.com>
2025-10-19 19:02:53 +0100

    small conn changes

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -1,5 +1,30 @@
 import mongoose from "mongoose";
 
-const DB_URL = process.env.MONGODB_URL;
+declare global {
+  // extend NodeJS.Global
+  var mongooseCache: {
+    conn: mongoose.Mongoose | null;
+    promise: Promise<mongoose.Mongoose> | null;
+  };
+}
+let DB_URL: string;
+if (process.env.MONGODB_URL) {
+  DB_URL = process.env.MONGODB_URL;
+} else {
+  throw new Error("Please define a MongoDB URL env variable");
+}
 
-if (!DB_URL) throw new Error("Please define a MongoDB URL env variable");
+if (!global.mongooseCache) {
+  global.mongooseCache = { conn: null, promise: null };
+}
+
+export async function connectDB() {
+  if (global.mongooseCache.conn) return global.mongooseCache.conn;
+
+  if (!global.mongooseCache.promise) {
+    global.mongooseCache.promise = mongoose.connect(DB_URL).then((m) => m);
+  }
+
+  global.mongooseCache.conn = await global.mongooseCache.promise;
+  return global.mongooseCache.conn;
+}
